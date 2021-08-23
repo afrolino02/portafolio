4fa9dcbcf3db319cc8eb2e67cb505f429d0855e3
afrolino02 <andres.puello.gomez@gmail.com>
2021-08-23 10:54:00 -0500

    Primera carga

---
+++ script.js
@@ -0,0 +1,38 @@
+ScrollReveal().reveal('.scroll', { delay: 500 });
+
+// Scroll to specific values
+// scrollTo is the same
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+//Get the button:
+mybutton = document.getElementById("ir-arriba");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+ScrollReveal().reveal('.scroll', { delay: 2000 });
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 2000 });
+ScrollReveal().reveal('.d-block');
+ScrollReveal().reveal('.nada', { delay: 500 });
