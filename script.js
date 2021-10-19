08ff03b20b357f9a8ea496b799e93c977a5b36f8
afrolino02 <andres.puello.gomez@gmail.com>
2021-10-19 14:29:16 -0500

    quinta carga

--- script.js
+++ script.js
@@ -31,3 +31,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
